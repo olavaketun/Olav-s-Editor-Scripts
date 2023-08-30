4f9bd78f58220769ac349dc95c59cee1de32a669
Olav Aketun <olav.aketun@qispace.com>
2023-08-30 12:52:08 +0200

    UPDATE picking script

--- Custom Scripts/Picking.cs
+++ Custom Scripts/Picking.cs
@@ -7,22 +7,22 @@ public class Picking : MonoBehaviour
 
 {
 	[MenuItem("Tools/Picking/Disable Picking")]
-	static void DisablePicking()
+	private static void DisablePicking()
 	{
-		//Record the action so undo works
-		Undo.RecordObject(selectedObject, "Disable Picking");
 		//Array of selected objects
 		GameObject[] selectedObject = Selection.gameObjects;
+		//Record the action so undo works
+		Undo.RecordObject(selectedObject, "Disable Picking");
 		//Disable picking for the selected objects in the array
 		SceneVisibilityManager.instance.DisablePicking(selectedObject, true);
 	}
 	[MenuItem("Tools/Picking/Enable Picking")]
-	static void EnablePicking()
+	private static void EnablePicking()
 	{
-		//Record the action so undo works
-		Undo.RecordObject(selectedObject, "Enable Picking");
 		//Array of selected objects
 		GameObject[] selectedObject = Selection.gameObjects;
+		//Record the action so undo works
+		Undo.RecordObject(selectedObject, "Enable Picking");
 		//Enable picking for the selected objects in the array
 		SceneVisibilityManager.instance.EnablePicking(selectedObject, true);
 	}
